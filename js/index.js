531e40215de00e788905d449c8c3e35c85eac133
DimaKlan <dima.klan.99@mail.ru>
2022-01-14 10:27:20 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// БУРГЕР МЕНЮ
+
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (menuIcon) {
+    menuIcon.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
+// ПЛАВНАЯ ПРОКРУТКА
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            // ДЛЯ МЕНЮ НА ТЕЛЕФОНЕ, ЧТОБ СКРЫВАЛОСЬ ОКНО И ПРОЛИСТЫВАЛОСЬ ТУДА КУДА НАДО
+            if(menuIcon.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                menuIcon.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+
+}
